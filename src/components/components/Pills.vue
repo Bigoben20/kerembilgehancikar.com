c00f4e453c3155b152adc54758cb21909d8cc11a
Kerem Cikar <k.bilge3817@gmail.com>
2024-04-29 23:21:18 +0300

    anasayfa done

--- src/components/components/Pills.vue
+++ src/components/components/Pills.vue
@@ -2,6 +2,15 @@
   <div v-if="tag == 'completed'" class="bg-green-500 px-1.5 py-[1px] rounded-full text-[11px] w-fit uppercase font-semibold tracking-wide">
     Completed
   </div>
+  <div v-else-if="tag == 'now'" class="bg-amber-500 px-1.5 py-[1px] rounded-full text-[11px] w-fit uppercase font-semibold tracking-wide">
+    NOW
+  </div>
+  <div v-else-if="tag == 'paused'" class="bg-pink-500 px-1.5 py-[1px] rounded-full text-[11px] w-fit uppercase font-semibold tracking-wide">
+    PAUSED
+  </div>
+  <div v-else-if="tag == 'personal'" class="bg-stone-500 px-1.5 py-[1px] rounded-full text-[11px] w-fit uppercase font-semibold tracking-wide">
+    PERSONAL
+  </div>
 </template>
 
 <script setup>
--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -4,9 +4,18 @@
             <p>Projects</p>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3">
-            <a :href="project.url" target="_blank" v-for="project in projects" :key="project.id" class="relative card">
-                <Pills :tag="project.tag" class="absolute top-6 left-6 "/>
-                <div class="w-full h-40 rounded-lg bg-neutral-200"></div>
+            <a :href="project.url" target="_blank" v-for="project in projects" :key="project.id" class="card">
+                
+                <div v-if="project.img" class="relative w-full overflow-hidden rounded-lg bg-neutral-200">
+                    <Pills :tag="project.tags[0]" class="absolute top-2 left-2 "/>
+                    
+                    <div class="absolute bottom-2 right-2 bg-stone-500 w-6 h-6 rounded-full text-[11px] flex items-center justify-center tooltip">
+                        <i class="fa-solid" :class="project.tags[1] == 'personal'?'fa-user':'fa-briefcase'"></i>
+                        <span class="capitalize tooltiptext">{{ project.tags[1] }}</span>
+                    </div>
+                    <img :src="`${project.img}`" alt="" />
+                </div>
+                <div v-else class="w-full h-40 rounded-lg bg-neutral-200"></div>
                 <span class="text-lg font-semibold break-words">
                     {{ project.title }}
                     <i class="text-blue-500 fa-solid fa-link"></i>
@@ -38,8 +47,8 @@ const projects = ref([
         technologies:"HTML, CSS, PHP, JS, jQuery, Curl",
         description: "A website for obtaining travel insurance quotes and making insurance sales.",
         url: "https://www.seyahatpolicesi.com",
-        img: "",
-        tag: "completed"
+        img: "/src/assets/images/projects/sepo.jpeg",
+        tags: ["completed","job"]
     },
     {
         id: 2,
@@ -47,8 +56,8 @@ const projects = ref([
         technologies:"TailwindCSS, AlpineJS, Laravel, Livewire",
         description: "The dashboard page for Tamamliyo company, where partners, sales, and other controls can be managed. Ongoing developments are in progress.",
         url: "https://dashboard.tamamliyo.com",
-        img: "",
-        tag: "completed"
+        img: "/src/assets/images/projects/dashboard.jpeg",
+        tags: ["completed","job"]
     },
     {
         id: 3,
@@ -56,8 +65,8 @@ const projects = ref([
         technologies: "HTML, CSS, PHP, JS, jQuery, Curl, TailwindCSS, AlpineJS",
         description: "A product sales page integrated into the Tamamliyo company website. Quotes can be received and sales can be made on this platform.",
         url: "https://tamamliyo.com",
-        img: "",
-        tag: "completed"
+        img: "/src/assets/images/projects/tamamliyo.jpeg",
+        tags: ["completed","job"]
     },
     {
         id: 4,
@@ -65,8 +74,26 @@ const projects = ref([
         technologies:"Vue.js, TailwindCSS",
         description: "A platform where mobile phone dealers can sell mobile phone insurance for additional income. Ongoing developments are in progress.",
         url: "https://kirilmiyo.com",
-        img: "",
-        tag: "completed"
+        img: "/src/assets/images/projects/kirilmiyo.jpeg",
+        tags: ["completed","job"]
+    },
+    {
+        id: 5,
+        title: "Vue Web App",
+        technologies:"Vue.js, TailwindCSS, Laravel, RestAPI",
+        description: "A site where current exchange rates and news can be obtained through a free API service.",
+        url: "https://github.com/Bigoben20/vue_dashboard",
+        img: "/src/assets/images/projects/web_app.jpeg",
+        tags: ["paused","personal"]
+    },
+    {
+        id: 6,
+        title: "My FRP Characters",
+        technologies:"Vue.js, TailwindCSS, Laravel, Inertia",
+        description: "A website for creating, editing, and deleting accounts and characters for a desktop role-playing game.",
+        url: "https://github.com/Bigoben20/my_frp_characters",
+        img: "/src/assets/images/projects/frp_characters.jpeg",
+        tags: ["paused","personal"]
     },
 ]);
 </script>
