e3db69abc8dce5ba79a36e6c9faa725cd3ec6e11
Kerem Cikar <k.bilge3817@gmail.com>
2024-05-06 18:21:19 +0300

    Update Projects.vue

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -43,7 +43,7 @@ import Pills from "@/components/components/Pills.vue"
 import sepoImg from "@/assets/images/projects/sepo.jpeg"
 import dashboardImg from "@/assets/images/projects/dashboard.jpeg"
 import tamamliyoImg from "@/assets/images/projects/tamamliyo.jpeg"
-import kirilmiyoImg from "@/assets/images/projects/tamamliyo.jpeg"
+import kirilmiyoImg from "@/assets/images/projects/kirilmiyo.jpeg"
 import webApp from "@/assets/images/projects/web_app.jpeg"
 import frpCharactersImg from "@/assets/images/projects/frp_characters.jpeg"
 
