dee3ae777fcfd94f98bb5faad697dc8c4b9f0bec
Kerem Cikar <k.bilge3817@gmail.com>
2024-05-14 20:29:16 +0300

    Update Footer.vue

--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -5,7 +5,7 @@
             © Kerem Bilgehan Çıkar - 2024
         </span>
 
-        <button class="py-1 px-2 bg-green-900 rounded-full group text-[10px] leading-none" @click="goTop()">
+        <button class="py-1 px-2 bg-green-900 rounded-full group text-[10px] leading-none flex items-center" @click="goTop()">
             <span class="hidden sm:block">Back to top</span>
             <i class="sm:ml-2 transition-all duration-100 ease-out fa-solid fa-arrow-up group-hover:-translate-y-0.5"></i>
         </button>
